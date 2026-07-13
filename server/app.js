3b9451828cc1713c28318b943df52785dec17958
kanakmi2007 <kanakmi2007@gmail.com>
2026-07-13 23:26:00 +0530

    Connect MongoDB database

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 // Middleware
 app.use(cors());
@@ -9,7 +15,7 @@ app.use(express.json());
 
 // Test Route
 app.get("/", (req, res) => {
-    res.send("🚀 Smart Expense Manager Backend is Running!");
+  res.send("🚀 Smart Expense Manager Backend is Running!");
 });
 
 module.exports = app;
